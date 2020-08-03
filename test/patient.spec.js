5e628873f125aec883335fadccc4364c46ee97f2
PATRICK BARRETO DOS SANTOS <barretodsp@gmail.com>
2020-08-03 00:21:31 -0300

    Tests for patient/create  done

--- test/patient.spec.js
+++ test/patient.spec.js
@@ -8,6 +8,9 @@ const chai = require("chai");
 const beforeTest = require('../commons/tests/before_scripts')
 const expect = chai.expect;
 const urlBase = process.env.API_BASE;
+const { uuid } = require('uuidv4');
+const pcid = uuid()
+
 const validCreate = {
   last_name: "Postman",
   first_name: "OLL",
@@ -25,14 +28,29 @@ const invalidCreate = {
   contacts: null
 }
 
+const validUpdate = {
+  patient_id: pcid,
+  last_name: "ALAL",
+  first_name: "OLL",
+  blood_type: "AB-",
+}
+
+const invalidUpdate = {
+  patient_id: pcid,
+  last_name: null,
+  first_name: "OLL",
+  blood_type: "AB-",
+}
 
 describe("Patient Endpoints", function () {
   before("", async function () {
     await beforeTest.clean()
-    await beforeTest.createPatientAndContact()
   })
 
   describe("GET#/patient/getAll", function () {
+    before("", async function () {
+      await beforeTest.createPatientAndContact()
+    })
     it("Return all Patients and Contacts", function (done) {
       request.get({ url: urlBase + '/api/v1/patient/getAll', json: { algo: 'ok' } }, function (error, response, body) {
         expect(response.statusCode).to.equal(200);
@@ -56,6 +74,27 @@ describe("Patient Endpoints", function () {
     it("When params are invalids, Bad Request ", function (done) {
       request.post({ url: urlBase + '/api/v1/patient/create', json: invalidCreate }, function (error, response, body) {
         expect(response.statusCode).to.equal(400);
+        expect(body).to.have.property('error');
+        done();
+      });
+    });
+  })
+
+  describe("POST#/patient/update", function () {
+    before("", async function () {
+      await beforeTest.createPatient(pcid)
+    })
+    it("When params are valids, Update Patient", function (done) {
+      request.post({ url: urlBase + '/api/v1/patient/update', json: validUpdate }, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.have.property('data');
+        done();
+      });
+    });
+
+    it("When params are invalids, Do nothing ", function (done) {
+      request.post({ url: urlBase + '/api/v1/patient/update', json: invalidUpdate }, function (error, response, body) {
+        expect(response.statusCode).to.equal(500);
         expect(body).to.have.property('error');
         done();
       });
